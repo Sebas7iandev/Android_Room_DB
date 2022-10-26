2a6232721d506b5fb3a54194c7aa406482048a18
sebastian <sevy2103@gmail.com>
2022-10-26 00:42:05 -0500

    Initial commit - Create initial app

---
+++ app/src/main/java/com/androidapps/codelabs/roomword/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidapps.codelabs.roomword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
